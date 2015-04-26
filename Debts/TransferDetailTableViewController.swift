24e2d80d56a2b79c8e1f85e8a772e8a3c060d9c0
Anna Muenster <anna.muenster@web.de>
2015-04-26 18:21:03 +0200

    peopleBtns for TransferDetail

--- Debts/TransferDetailTableViewController.swift
+++ Debts/TransferDetailTableViewController.swift
@@ -15,6 +15,8 @@ class TransferDetailTableViewController: UITableViewController {
     @IBOutlet weak var transferName: UILabel!
     @IBOutlet weak var transferAmount: UILabel!
     @IBOutlet weak var transferNotes: UITextView!
+    @IBOutlet weak var payerView: UIView!
+    @IBOutlet weak var participantView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +31,22 @@ class TransferDetailTableViewController: UITableViewController {
             if let notes = self.transferNotes {
                 notes.text = transfer.notes
             }
+            let btnY:CGFloat = 15
+            let btnSize:CGFloat = 40
+            var btnX:CGFloat = 20
+            if let payer = self.payerView {
+                var btn = PeopleButton(frame: CGRectMake(btnX, btnY, btnSize, btnSize), title: transfer.userPayed.getName())
+                payer.addSubview(btn)
+            }
+            
+            if let participants = self.participantView {
+                for user in transfer.participants {
+                    var btn = PeopleButton(frame: CGRectMake(btnX, btnY, btnSize, btnSize), title: user.user.getName())
+                    participants.addSubview(btn)
+                    btnX += btnSize + btnSize/2
+                }
+            }
+            
         }
     }
 
