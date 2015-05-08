8776e13c66c6871269cccf857e88cc4243cc52de
anna <anna.muenster@web.de>
2015-05-08 13:33:42 +0200

    empty AddGroup-View with segue to AddContact

---
+++ Debts/AddGroupTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddGroupTableViewController.swift
+//  Debts
+//
+//  Created by Anna on 08.05.15.
+//  Copyright (c) 2015 Anna Muenster. All rights reserved.
+//
+
+import UIKit
+
+class AddGroupTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
