eef9d91e580346cbe8f6e4e982ee131b6fbc6db5
Anna Muenster <anna.muenster@web.de>
2015-06-21 11:32:10 +0200

    sections in usertableview

--- app/Debts/Controllers/UserTableViewController.swift
+++ app/Debts/Controllers/UserTableViewController.swift
@@ -15,9 +15,10 @@ import AddressBook
 
 class UserTableViewController: UITableViewController {
     
-    var myContacts = [SwiftAddressBookPerson]()
     let groupOverviewIdentifier = "groupOverview"
     let userCellIdentifier = "userCell"
+    var contactSections = [[SwiftAddressBookPerson]]()
+    var sectionNames = [String]()
     
     // MARK: - View Set Up
     
@@ -26,7 +27,8 @@ class UserTableViewController: UITableViewController {
         
         // load people and display them in tableview
         self.loadPeopleFromAddressBook({(people) -> Void in
-            self.myContacts = people
+            let myContacts:[SwiftAddressBookPerson] = people
+            self.createSectionsArray(myContacts)
             self.tableView.reloadData()
         })
         
@@ -55,17 +57,33 @@ class UserTableViewController: UITableViewController {
     // MARK: - Table view data source & Delegate
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return self.contactSections.count
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sectionNames[section]
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.myContacts.count
+     //   return self.myContacts.count
+        return self.contactSections[section].count
     }
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(userCellIdentifier, forIndexPath: indexPath) as! UITableViewCell
-        var person: User = User(person: myContacts[indexPath.row])
-        cell.textLabel?.text = "\(person.firstname) \(person.lastname)"
+        
+        let user = self.contactSections[indexPath.section][indexPath.row]
+        var person: User = User(person: user)
+        //var person: User = User(person: myContacts[indexPath.row])
+        
+        let labelText = "\(person.firstname) \(person.lastname)"
+        let highlightRange = (labelText as NSString).rangeOfString(person.firstname)
+        // create attributed string so that lastname is displayed in bold
+        let attributedString = NSMutableAttributedString(string: labelText, attributes:[NSFontAttributeName : UIFont.systemFontOfSize(17.0)])
+        attributedString.setAttributes([NSFontAttributeName : UIFont.boldSystemFontOfSize(17)], range: highlightRange)
+        
+        cell.textLabel?.attributedText = attributedString
         cell.textLabel?.textColor = UIColor.darkGrayColor()
         
         return cell
@@ -73,7 +91,7 @@ class UserTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let cell = tableView.dequeueReusableCellWithIdentifier(userCellIdentifier, forIndexPath: indexPath) as! UITableViewCell
-        var person: User = User(person: myContacts[indexPath.row])
+        var person: User = User(person: self.contactSections[indexPath.section][indexPath.row])
         GlobalVar.currentUser = person
         
         // get user details or if it does not exist, create a new on and proceed
@@ -98,5 +116,44 @@ class UserTableViewController: UITableViewController {
     func proceedWithSelectedUser(uid: String) {
         GlobalVar.currentUid = uid
         self.performSegueWithIdentifier(groupOverviewIdentifier, sender: self)
+    }
+    
+    func sortNames(s1: String, s2: String) -> Bool {
+        return s1 < s2
+    }
+    
+    func createSectionsArray(fetchedContacts: [SwiftAddressBookPerson]) {
+        var myContacts = fetchedContacts //mutable copy
+        myContacts.sort({$0.firstName?.uppercaseString < $1.firstName?.uppercaseString})
+        
+        var sectionLetter = ""
+        var sectionIndex = -1
+        
+        self.contactSections = []
+        self.sectionNames = []
+        
+        for contact in myContacts {
+            if let first = contact.firstName {
+                let firstLetter = String(Array(first)[0])
+                
+                if sectionLetter != firstLetter {
+                    sectionNames.append(firstLetter)
+                    contactSections.append([])
+                    sectionIndex++
+                    sectionLetter = firstLetter
+                }
+            } else {
+                let alternateLetter = "#"
+                if let index = find(sectionNames, alternateLetter) {
+                    sectionIndex = index
+                } else{
+                    sectionNames.append(alternateLetter)
+                    contactSections.append([])
+                    sectionIndex++
+                    sectionLetter = alternateLetter
+                }
+            }
+            contactSections[sectionIndex].append(contact)
+        }
     }
 }
